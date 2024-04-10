bf15195d3ee01faea94de870538b5e5c67e64bfe
Barsegyan-Arevik <arevikbarsegan5@gmail.com>
2024-04-10 19:12:53 +0300

    Renamed the SQL query

--- backend/metrics/queries/postgres/video/play_video_count_daily.py
+++ backend/metrics/queries/postgres/video/play_video_count_daily.py
@@ -1,11 +1,11 @@
-from metrics.queries.postgres.sql_queries import SQL_QUERY_PLAY_VIDEO_TIMES
+from metrics.queries.postgres.sql_queries import SQL_QUERY_PLAY_VIDEO_COUNT_DAILY
 from metrics.utils.db_operations import execute_query_with_result
 from metrics.utils.file_operations import RESULT_PATH, generate_line_figure
 from metrics.utils.metric_operations import calc_course_metric
 
 
 def calculate_video_start_times(connection, course_id):
-    return execute_query_with_result(connection, SQL_QUERY_PLAY_VIDEO_TIMES, course_id)
+    return execute_query_with_result(connection, SQL_QUERY_PLAY_VIDEO_COUNT_DAILY, course_id)
 
 
 def main():
