22386332a5cf3deebf8d7b3c0e73cfb49bd842a6
Barsegyan-Arevik <arevikbarsegan5@gmail.com>
2024-04-12 14:21:31 +0300

    Added SQL query -- top responses

---
+++ backend/metrics/queries/postgres/forum/top_responses.py
@@ -0,0 +1,19 @@
+from metrics.queries.postgres.sql_queries import SQL_QUERY_TOP_RESPONSES
+from metrics.utils.db_operations import execute_query_with_result
+from metrics.utils.metric_operations import calc_course_metric
+
+
+def calculate_total_user_time_on_course(connection, course_id):
+    return execute_query_with_result(connection, SQL_QUERY_TOP_RESPONSES, course_id)
+
+
+def main():
+    total_users_time_on_course = calc_course_metric(
+        calculate_total_user_time_on_course,
+        "top_responses.csv",
+        ['author', 'thread_title', 'thread_body', 'total_comments', 'total_votes']
+    )
+
+
+if __name__ == '__main__':
+    main()
