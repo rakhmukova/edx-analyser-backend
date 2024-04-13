b30ddc003824737ed2480a0d45837d0bb147cfcd
rakhmukova <lessness19@gmail.com>
2024-04-13 21:47:19 +0200

    Change url

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -3,6 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # CourseIDConverter
-    path('courses/<str:course_id>/', include('metrics.api.endpoints')),
+    path('api/courses/<str:course_id>/', include('metrics.api.endpoints')),
 ]
