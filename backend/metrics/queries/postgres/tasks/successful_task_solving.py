3c693df9d5f7ecd949f5582e0ca831196b08cc23
Barsegyan-Arevik <arevikbarsegan5@gmail.com>
2024-04-13 13:40:33 +0300

    Replaced the problem_id with links

--- backend/metrics/queries/postgres/tasks/successful_task_solving.py
+++ backend/metrics/queries/postgres/tasks/successful_task_solving.py
@@ -1,17 +1,47 @@
 from metrics.queries.postgres.sql_queries import SQL_QUERY_CORRECTLY_SOLVED_PROBLEMS
-from metrics.utils.db_operations import execute_query_with_result
-from metrics.utils.metric_operations import calc_course_metric
+from metrics.utils.db_operations import execute_query_with_result, open_db_connection, close_db_connection
+from metrics.utils.file_operations import save_output_to_file
+from metrics.utils.metric_operations import DEFAULT_COURSE_ID
+from metrics.utils.url_operations import remove_parameters_from_url
+
+def calculate_pages(connection, course_id):
+    return process_urls(execute_query_with_result(connection, SQL_QUERY_CORRECTLY_SOLVED_PROBLEMS, course_id))
 
 
-def calculate_total_user_time_on_course(connection, course_id):
-    return execute_query_with_result(connection, SQL_QUERY_CORRECTLY_SOLVED_PROBLEMS, course_id)
+def process_urls(result):
+    urls_with_counts = {}
+    for item in result:
+        video_id = item[0]
+        url = remove_parameters_from_url(item[1])
+        all_attempts = item[2]
+        correct_attempts = item[3]
+
+        # Учитываем ссылки с подстрокой "xblock"
+        if 'xblock' in url:
+            # Ищем ссылку без параметров для той же video_id
+            for key, value in urls_with_counts.items():
+                if key == video_id and 'xblock' not in value[0]:
+                    urls_with_counts[key][1] = all_attempts
+                    urls_with_counts[key][2] = correct_attempts
+                    break
+        else:
+            if video_id in urls_with_counts:
+                urls_with_counts[video_id][1] = all_attempts
+                urls_with_counts[video_id][2] = correct_attempts
+            else:
+                urls_with_counts[video_id] = [url, all_attempts, correct_attempts]
+
+    return list(urls_with_counts.values())
+
+
+
 
 def main():
-    total_users_time_on_course = calc_course_metric(
-        calculate_total_user_time_on_course,
-        "successful_problem_solving.csv",
-        ['problem_id','all_attempts', 'successful_attempts']
-    )
+    connection = open_db_connection()
+    pages_urls = calculate_pages(connection, course_id=DEFAULT_COURSE_ID)
+    headers = ['all_attempts', 'correct_attempts']
+    save_output_to_file("successful_problem_solving.csv", pages_urls, headers)
+    close_db_connection(connection)
 
 
 if __name__ == '__main__':
