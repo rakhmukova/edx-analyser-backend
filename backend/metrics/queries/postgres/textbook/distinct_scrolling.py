293212abce3230efc17fa9ac1cd845130052e2bb
Barsegyan-Arevik <arevikbarsegan5@gmail.com>
2024-04-13 14:21:29 +0300

    The path of the resulting file has changed

--- backend/metrics/queries/postgres/textbook/distinct_scrolling.py
+++ backend/metrics/queries/postgres/textbook/distinct_scrolling.py
@@ -1,6 +1,5 @@
 from metrics.queries.postgres.sql_queries import SQL_QUERY_DISTINCT_SCROLLING
 from metrics.utils.db_operations import execute_query_with_result
-from metrics.utils.file_operations import generate_bar_figure
 from metrics.utils.metric_operations import calc_course_metric
 
 
@@ -9,15 +8,13 @@ def unique_views_of_available_pdf(connection, course_id):
 
 
 def main():
-    result_file = "distinct_pdf_scrolling.csv"
-    fields = ['pdf_name', 'scrolling_amount']
+    result_file = "textbook/scrolling_time.csv"
+    fields = ['username', 'time']
     calc_course_metric(
         unique_views_of_available_pdf,
         result_file,
         fields
     )
-    generate_bar_figure(result_file, fields, xaxis_title='Название PDF', yaxis_title='Количество прокруток')
-
 
 if __name__ == '__main__':
     main()
--- backend/metrics/queries/postgres/textbook/searched_pdf_terms.py
+++ backend/metrics/queries/postgres/textbook/searched_pdf_terms.py
@@ -1,6 +1,5 @@
 from metrics.queries.postgres.sql_queries import SQL_QUERY_SEARCHED_PDF_TERMS
 from metrics.utils.db_operations import execute_query_with_result
-from metrics.utils.file_operations import generate_bar_figure
 from metrics.utils.metric_operations import calc_course_metric
 
 
@@ -9,14 +8,13 @@ def unique_views_of_available_pdf(connection, course_id):
 
 
 def main():
-    result_file = "searched_pdf_terms.csv"
+    result_file = "textbook/searched_pdf_terms.csv"
     fields = ['count_number', 'word']
     calc_course_metric(
         unique_views_of_available_pdf,
         result_file,
         fields
     )
-    generate_bar_figure(result_file, fields)
 
 
 if __name__ == '__main__':
