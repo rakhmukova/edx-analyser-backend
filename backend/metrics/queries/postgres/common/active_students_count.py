2860ffcdb627c8a16d21ba4d00552d8d56ca8643
rakhmukova <lessness19@gmail.com>
2024-04-16 12:30:29 +0200

    Fix formatting

--- backend/metrics/queries/postgres/common/active_students_count.py
+++ backend/metrics/queries/postgres/common/active_students_count.py
@@ -6,6 +6,7 @@ from metrics.utils.metric_operations import calc_course_metric
 def calc_all_users_count(connection, course_id):
     return execute_query_with_result(connection, SQL_QUERY_ACTIVE_STUDENTS_COUNT, course_id)
 
+
 def main():
     calc_course_metric(
         calc_all_users_count,
