15aff56e099c5ae44f00a9b756a46ddf418bef8e
Barsegyan-Arevik <arevikbarsegan5@gmail.com>
2024-05-06 10:23:44 +0300

    Fixed queries

--- backend/metrics/queries/postgres/textbook/textbook_popularity.py
+++ backend/metrics/queries/postgres/textbook/textbook_popularity.py
@@ -1,14 +1,14 @@
-from metrics.queries.postgres.sql_queries import SQL_QUERY_DISTINCT_SCROLLING
+from metrics.queries.postgres.sql_queries import SQL_QUERY_TEXTBOOK_POPULARITY
 from metrics.utils.db_operations import execute_query_with_result
 from metrics.utils.metric_operations import calc_course_metric
 
 
 def unique_views_of_available_pdf(connection, course_id):
-    return execute_query_with_result(connection, SQL_QUERY_DISTINCT_SCROLLING, (course_id,))
+    return execute_query_with_result(connection, SQL_QUERY_TEXTBOOK_POPULARITY, (course_id,))
 
 
 def main():
-    result_file = "textbook/scrolling_time.csv"
+    result_file = "textbook/textbook_popularity.csv"
     fields = ['pdf_name', 'views_count', 'unique_students_count']
     calc_course_metric(
         unique_views_of_available_pdf,
